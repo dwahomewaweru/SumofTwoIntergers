7fd8fd5367c35683dae4e2fb3912d0319d4d4517
Wahome97 <d.wahome@alustudent.com>
2021-10-26 18:25:51 +0300

    Add project files.

---
+++ SumofTwoIntergers/SumofTwoIntergers.cpp
@@ -0,0 +1,22 @@
+// SumofTwoIntergers.cpp 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+    cout << "Sum of two intergers input by user\n";
+    cout << "....................................\n";
+    cout << "Please input first number:";
+    cin >> num1;
+    cout << "\n\n";
+    cout << "Please input second number: ";
+    cin >> num2;
+    sum = num1 + num2;
+    cout << "Sum of the two numbers is : " << sum;
+    cout << "\n\n";
+
+    return 0;
+
+}
